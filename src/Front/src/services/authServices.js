9a84bfda61c7a7ca5766a6f5ed7c1564b4c9225d
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-04-19 21:07:26 +0200

    feat: login, register and redux

---
+++ src/Front/src/services/authServices.js
@@ -0,0 +1,37 @@
+const URL = "https://h8ter.zeabur.app/api/auth"
+
+export const RegisterUser = async (user) => {
+  try {
+    const response = await fetch(`${URL}/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+    const data = await response.json()
+    if (!data.success) {
+      throw new Error(data.message)
+    }
+    return data
+  } catch (error) {
+    console.log("Error al registrar el usuario", error)
+    throw error
+  }
+}
+export const LoginUser = async (user) => {
+  try {
+    const response = await fetch(`${URL}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+    const data = await response.json()
+    if (!data.success) {
+      throw new Error(data.message)
+    }
+    return data
+  } catch (error) {
+    console.log("Error al iniciar sesión", error)
+    throw error
+  }
+}
+
