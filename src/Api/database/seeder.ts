55d24ce179e2dfe58ea81416a09cc9c0f5eb1f89
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-05-12 12:14:55 +0200

    fixed seeder

--- src/Api/database/seeder.ts
+++ src/Api/database/seeder.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose"
 import User from "../entities/user/model"
 import Game from "../entities/game/model"
 import Category from "../entities/category/model"
+import Address from "../entities/address/model"
 import dotenv from "dotenv"
 dotenv.config()
 
@@ -26,6 +27,9 @@ export const seed = async (rutaArchivo: string, modelo: any) => {
     if (objeto.author && objeto.author.$oid) {
       objeto.author = new mongoose.Types.ObjectId(objeto.author.$oid)
     }
+    if (objeto.owner && objeto.owner.$oid) {
+      objeto.owner = new mongoose.Types.ObjectId(objeto.owner.$oid)
+    }
     if (objeto.category && objeto.category.$oid) {
       objeto.category = new mongoose.Types.ObjectId(objeto.category.$oid)
     }
@@ -42,21 +46,21 @@ mongoose
   .connect(process.env.MONGO_URL as string)
   .then(async () => {
     // limpiar la base de datos
-    await Promise.all([
-      User.deleteMany({}),
-      Game.deleteMany({}),
-      Category.deleteMany({}),
-    ])
+    await User.deleteMany({})
+    await Game.deleteMany({})
+    await Category.deleteMany({})
+    await Address.deleteMany({})
 
     const archivosModelos = [
       { ruta: "./Json/test.users.json", modelo: User },
-      { ruta: "./Json/test.games.json", modelo: Game },
       { ruta: "./Json/test.categories.json", modelo: Category },
+      { ruta: "./Json/test.games.json", modelo: Game },
+      { ruta: "./Json/test.addresses.json", modelo: Address },
     ]
 
     for (const { ruta, modelo } of archivosModelos) {
       const rutaAbsoluta = path.join(__dirname, ruta)
-      seed(rutaAbsoluta, modelo).catch(console.error)
+      await seed(rutaAbsoluta, modelo).catch(console.error)
     }
   })
   .catch((error) =>
